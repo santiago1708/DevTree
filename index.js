48f1f7cd67f18ddf84644917899b2a1eb680b59a
santiago1708 <vela123tique@gmail.com>
2025-04-16 12:16:51 -0500

    inicio de proyecto DevTree

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo desde DevTree');
+const express = require('express')
+
+const app = express() //instancia del servidor
+
+
+app.get('/', (req, res) => { //Req = Enviando mucha información al servidor, Res = Respuesta del servidor
+    res.send('Hola mundo')
+})
+
+app.listen(3000, () => {
+    console.log('Server on port 3000')
+})
