8717725ec05ef249a394ec77c369d3d92e7daa26
santiago1708 <vela123tique@gmail.com>
2025-11-12 22:59:18 -0500

    Modelo para base de datos User

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,13 @@
 import mongoose, { mongo } from "mongoose";
+import colors from "colors";
 
 export const connectDB = async () => {
     try {
         const {connection} = await mongoose.connect(process.env.MONGO_URI)
         const url = `${connection.host} : ${connection.port}`
-        console.log(`MongoDB connected in: ${url}`);
+        console.log(colors.bgCyan.bold (`MongoDB connected in: ${url}`));
     } catch (error) {
-        console.log('Error connecting to MongoDB:', error.message);
+        console.log(colors.bgRed.bold(`Error connecting to MongoDB: ${error.message}`));
         process.exit(1);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import server from './server'
 const port = process.env.port || 3000 //puerto del servidor
 
 server.listen(port, () => {
-    console.log('Server on port:', port)
+    console.log(colors.bgGreen.bold (`Server on port: ${port}`))
 })
