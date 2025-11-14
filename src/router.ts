fd4538946e4097620b1f092dc90560636d8b7892
santiago1708 <vela123tique@gmail.com>
2025-11-13 23:08:01 -0500

    expressValidator: Validacion user

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createAccount } from './handlers';
 
 const router = Router();
 
 //Autenticacion y Registro
 
-router.post('/auth/register', createAccount)
+router.post('/auth/register', 
+    body('handle')
+        .notEmpty()
+        .withMessage('El nombre de usuario es obligatorio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('El email no es valido'),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage('La contraseña debe tener al menos 8 caracteres'),
+    createAccount)
 
 export default router; //Exportando el router
