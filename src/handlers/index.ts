766a5989938c27be941c45049a27450c5d951b00
santiago1708 <vela123tique@gmail.com>
2025-11-13 22:36:00 -0500

    hash password user

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req : Request, res : Response) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
         const error = new Error('El usuario ya existe');
         res.status(409).json({ error: error.message });
         return
     }
-    await User.create(req.body)
+    const user = new User(req.body)
+    user.password = await hashPassword(password);
+    await user.save();
     res.status(201).send('Usuario registrado correctamente');
 } 
