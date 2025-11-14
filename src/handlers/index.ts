b997b993511c45fcce0a88d0d30bcb5c25898070
santiago1708 <vela123tique@gmail.com>
2025-11-13 22:49:33 -0500

    handle unico para los usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import slug from 'slug';
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
@@ -10,8 +11,22 @@ export const createAccount = async (req : Request, res : Response) => {
         res.status(409).json({ error: error.message });
         return
     }
+
+    const handle = slug(req.body.handle, '');
+
+    const handleExist = await User.findOne({ handle });
+    if (handleExist) {
+        const error = new Error('Nombre de usuario no disponible');
+        res.status(409).json({ error: error.message });
+        return
+    }
+    
+
     const user = new User(req.body)
     user.password = await hashPassword(password);
+    user.handle = handle;
+
     await user.save();
     res.status(201).send('Usuario registrado correctamente');
 } 
+
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
 interface IUser {
+    handle: string;
     name: string;
     email: string;
     password: string;
 }
 
 const userSchema = new Schema({
+    handle : {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+
     name : { 
         type: String,
         required: true,
@@ -16,7 +25,8 @@ const userSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password : {
         type: String,
