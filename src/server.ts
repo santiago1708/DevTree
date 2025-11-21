a997ba32caa3aa23c7994f4d83ec5039eb568d34
santiago1708 <vela123tique@gmail.com>
2025-11-20 20:55:31 -0500

    correciones

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express' //importando express
+import cors from "cors"
 import 'dotenv/config' //importando dotenv para leer el archivo .env
 import router from './router'
 import { connectDB } from './config/db'
+import { corsConfig } from './config/cors'
 
+connectDB() //Conectar a la base de datos
 
 const app = express() //instancia del servidor
-connectDB() //Conectar a la base de datos
+//cors
+app.use(cors(corsConfig))
 
 //Leer datos de formulario
 app.use(express.json())
