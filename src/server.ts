2f118ca342128ee90aec8508accb957400dbfdff
santiago1708 <vela123tique@gmail.com>
2025-11-20 21:24:35 -0500

    CORS y correciones en los handlers

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express' //importando express
 import 'dotenv/config' //importando dotenv para leer el archivo .env
+import cors from 'cors'
 import router from './router'
 import { connectDB } from './config/db'
+import { corsConfig } from './config/cors'
 
+connectDB() //Conectar a la base de datos
 
 const app = express() //instancia del servidor
-connectDB() //Conectar a la base de datos
+
+//CORS
+app.use(cors(corsConfig))
 
 //Leer datos de formulario
 app.use(express.json())
