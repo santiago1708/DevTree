915f0beb3abe20b841ac3b344a6ab60280c42aa0
santiago1708 <vela123tique@gmail.com>
2025-12-01 22:13:10 -0500

    se paso el getuser a un middleware

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -60,34 +60,5 @@ export const login = async (req: Request, res: Response) => {
 }
 
 export const getUser = async (req: Request, res: Response) => {
-    const bearer = req.headers.authorization;
-    if (!bearer) {
-        const error = new Error('No autorizado');
-        res.status(401).json({ error: error.message });
-        return
-    }
-
-    const [, token] = bearer.split(' ');
-
-    if (!token) {
-        const error = new Error('No autorizado');
-        res.status(401).json({ error: error.message });
-        return
-    }
-
-    try {
-        const result = jwt.verify(token, process.env.JWT_SECRET)
-        if (typeof result === 'object' && result.id) {
-            const user = await (await User.findById(result.id).select('-password -__v'));
-            if (!user) {
-                const error = new Error('El usuario no existe');
-                res.status(404).json({ error: error.message });
-                return
-            }
-            res.json(user)
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Token no valido' });
-        return
-    }
+    res.send(req.user)
 }
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body } from 'express-validator';
 import { createAccount, getUser, login } from './handlers';
 import { handleInpoutErrors } from './middleware/validation';
+import { authenticate } from './middleware/Auth';
 
 const router = Router();
 
@@ -35,6 +36,6 @@ router.post('/auth/login',
     login
 )
 
-router.get('/user', getUser)
+router.get('/user', authenticate,  getUser)
 
 export default router; //Exportando el router
